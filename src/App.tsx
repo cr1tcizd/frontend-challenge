5c482787394e30244853a8e330d2209ea730379b
Dmitriy Eliseev <dima.eliseev.200202@gmail.com>
2025-01-20 14:25:57 +0500

    fix errors

--- src/App.tsx
+++ src/App.tsx
@@ -6,9 +6,15 @@ function App() {
 	return (
 		<>
 			<Routes>
-				<Route path='/' element={<HomePage />} />
-				<Route path='/*' element={<Navigate to='/' replace />} />
-				<Route path='/favorite' element={<FavoritePage />} />
+				<Route path={`${import.meta.env.BASE_URL}`} element={<HomePage />} />
+				<Route
+					path={`${import.meta.env.BASE_URL}/*`}
+					element={<Navigate to={`${import.meta.env.BASE_URL}`} replace />}
+				/>
+				<Route
+					path={`${import.meta.env.BASE_URL}/favorite`}
+					element={<FavoritePage />}
+				/>
 			</Routes>
 		</>
 	)
